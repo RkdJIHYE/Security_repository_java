703cc57db8e2a437e665c5f89c34ce51fe120fa0
RkdJIHYE <kangbs210210@gmail.com>
2026-03-27 12:15:23 +0900

    Feat:예외처리 ServiceException에 추가

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -5,6 +5,7 @@ import com.back.domain.member.service.MemberService;
 import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.global.exception.ServiceException;
 import com.back.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -74,9 +75,8 @@ public class ApiV1PostController {
 
         Member actor = memberService.findByUsername(username).get();
 
-        //패스워드가 다를 경우 -> 중단
-        if (!password.equals(actor.getPassword())) {
-            throw new IllegalArgumentException("05-password-비밀번호가 일치하지 않습니다.");
+        if(!password.equals(actor.getPassword())) {
+            throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
         }
 
         Post post = postService.write(actor, reqBody.title, reqBody.content);
@@ -114,9 +114,7 @@ public class ApiV1PostController {
     @Transactional
     public RsData<PostModifyResBody> modify(
             @PathVariable int id,
-            @RequestBody @Valid PostModifyReqBody reqBody,
-            @RequestParam @NotBlank @Size(min=2, max=30) String username,
-            @RequestParam @NotBlank @Size(min=2,max=30) String password
+            @RequestBody @Valid PostModifyReqBody reqBody
     ) {
 
         Post post = postService.modify(id, reqBody.title, reqBody.content);
